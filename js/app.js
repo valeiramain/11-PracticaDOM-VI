1f053b2231c06d69c13f658003f43ee37fb7e148
Valentina Iramain <valentinairamain2020@gmail.com>
2025-05-07 11:08:46 -0300

    modificacmos el titul ode la 1er card usando innerHTMO

--- js/app.js
+++ js/app.js
@@ -11,3 +11,15 @@ function modificarTitulo(){
 
     // modificar el texto
 }
+
+function modificarTituloConInnerHTML(){
+    console.log('desde la funcion function modificarTituloConInnerHTML')
+    const titulo = document.getElementById('titulo')
+    titulo.innerHTML = 'texto usando <b>innerHTML</b>'
+}
+
+const btnCambiarTitulo = document.querySelector('#btnCambiarTituloInnerHTML')
+console.log(btnCambiarTitulo)
+
+//agregar un manejador de eventos
+btnCambiarTitulo.addEventListener('click',modificarTituloConInnerHTML)
